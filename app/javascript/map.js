2c993732edfeadbd24087b01d8656fdfc27f03c9
John Lude <johnwlude@gmail.com>
2023-06-18 16:46:05 -0400

    Add markers to map

--- app/javascript/map.js
+++ app/javascript/map.js
@@ -2,7 +2,7 @@ let map;
 
 async function initMap() {
     // The location of Uluru
-    const position = { lat: 37.7550307267667, lng: -122.384530734223 };
+    const position = { lat: 37.753966, lng: -122.447625 };
     // Request needed libraries.
     //@ts-ignore
     const { Map } = await google.maps.importLibrary("maps");
@@ -10,17 +10,31 @@ async function initMap() {
 
     // The map, centered at Uluru
     map = new Map(document.getElementById("map"), {
-        zoom: 15,
+        zoom: 12,
         center: position,
         mapId: "DEMO_MAP_ID",
     });
 
-    // The marker, positioned at Uluru
-    const marker = new google.maps.Marker({
-        map: map,
-        position: { lat: 37.7550307267667, lng: -122.384530734223 },
-        title: "Food Truck"
-    });
+    var xhr = new XMLHttpRequest();
+    xhr.open('GET', '/api/truck_data/map', true);
+    xhr.responseType = 'json';
+    xhr.onload = function() {
+        var status = xhr.status;
+        if (status === 200) {
+            xhr.response.forEach(function(truck, index) {
+                const marker = new google.maps.Marker({
+                    map: map,
+                    position: { lat: truck['latitude'] - 0, lng: truck['longitude'] - 0 },
+                    title: truck['name']
+                });
+            });
+        } else {
+            alert('Error loading food truck data');
+        }
+    };
+    xhr.send();
+
 }
 
 initMap();
+
