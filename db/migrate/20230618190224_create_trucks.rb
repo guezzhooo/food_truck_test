a5898eca6746f718cf6116eff02092b6a5cc576f
John Lude <johnwlude@gmail.com>
2023-06-18 16:43:58 -0400

    Truck model and migration.

---
+++ db/migrate/20230618190224_create_trucks.rb
@@ -0,0 +1,16 @@
+class CreateTrucks < ActiveRecord::Migration[7.0]
+  def change
+    create_table :trucks, id: false, primary_key: :location_id do |t|
+      t.integer :location_id
+      t.string :name
+      t.string :facility_type
+      t.string :location_description
+      t.string :address
+      t.string :status
+      t.text :food_items
+      t.decimal :latitude, precision: 20, scale: 10
+      t.decimal :longitude, precision: 20, scale: 10
+      t.timestamps
+    end
+  end
+end
